e3a565e0f7f32c1f037d873f1d1f5b12cdfdef84
Agapov Max <ddtpv@mail.ru>
2024-12-04 09:55:36 +0300

    Comment debug printf

--- prog/inf/task5/rational/rat_math.c
+++ prog/inf/task5/rational/rat_math.c
@@ -3,8 +3,8 @@
 
 // Функция суммирования
 rational_t rat_add(rational_t a, rational_t b) {
-    printf("Умножаем числитель a=%ld на знаменатель b=%ld и прибавляем умножение числителя b=%ld на знаменатель a=%ld\t = %ld\n", rational_numerator(a), rational_denominator(b), rational_numerator(b), rational_denominator(a), rational_numerator(a)*rational_denominator(b)+rational_numerator(b)*rational_denominator(a));
-    printf("И делим все это на произведение знаменателей %ld %ld = %ld\n", rational_denominator(a), rational_denominator(b), rational_denominator(a)*rational_denominator(b));
+    //printf("Умножаем числитель a=%ld на знаменатель b=%ld и прибавляем умножение числителя b=%ld на знаменатель a=%ld\t = %ld\n", rational_numerator(a), rational_denominator(b), rational_numerator(b), rational_denominator(a), rational_numerator(a)*rational_denominator(b)+rational_numerator(b)*rational_denominator(a));
+    //printf("И делим все это на произведение знаменателей %ld %ld = %ld\n", rational_denominator(a), rational_denominator(b), rational_denominator(a)*rational_denominator(b));
     return rational(rational_numerator(a)*rational_denominator(b)+rational_numerator(b)*rational_denominator(a),
     rational_denominator(a)*rational_denominator(b));
 }
