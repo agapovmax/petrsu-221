41e24f123a5b881235d265f7fc00fd7a05a3b135
Agapov Max <ddtpv@mail.ru>
2025-04-24 09:58:31 +0300

    Delete prog/inf/task5 directory

--- prog/inf/task5/rational/rat_math.h
+++
@@ -1,8 +0,0 @@
-#ifndef RATMATH_H
-#define RATMATH_H
-#include "rational.h"
-rational_t rat_add(rational_t a, rational_t b);
-rational_t rat_sub(rational_t a, rational_t b);
-rational_t rat_div(rational_t a, rational_t b);
-rational_t rat_mul(rational_t a, rational_t b);
-#endif
