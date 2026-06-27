e8956ccf8e01794d0a16ab82d7b3514043b6e04e
Максим Агапов <magapov@astralinux.ru>
2026-06-27 10:19:12 +0300

    Remove old algoritm

--- algoritm/README.md
+++
@@ -1,3 +0,0 @@
-### Алгоритмы и структуры данных
-
-[Лабораторные работы](https://petrsulabs.contest.codeforces.com/group/qeb9zA9PAI/contests)
--- algoritm/labs/lab3.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-a = list(map(int, input().split()))
-
-s = 0
-for i in range(n):
-    s = s + a[i] * (i + 1) * (n - i)
-
-print(s)
